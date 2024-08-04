f6e489f6c80a969a352c687331b2af96cdf8a685
Brian Hurlow <brian@brianhurlow.com>
2024-08-03 22:57:35 -0400

    make it work kind

--- main.ts
+++ main.ts
@@ -1,36 +1,37 @@
 #!/usr/bin/env node
 
-//import { prompt } from "./prompt";
-//
-//import yargs from "yargs/yargs";
-//
-//const argv = yargs(process.argv.slice(2))
-//  .options({
-//    a: { type: "boolean", default: false },
-//  })
-//  .parse();
-
+import fs from 'fs'
 import { ask } from './llm'
+import yargs from 'yargs/yargs'
 
-const input = `
-
-function cube(x) {
-  {{FILL_HERE}}
-}
-
-`
+const argv = yargs(process.argv.slice(2))
+  .options({
+    a: { type: 'boolean', default: false }
+  })
+  .parseSync()
 
 const main = async () => {
-  console.log('starting')
-  const res = await ask(input)
+  const [inputFile] = argv._
+
+  if (!inputFile) {
+    throw new Error('no file')
+  }
+
+  const exists = fs.existsSync(String(inputFile))
+
+  if (!exists) {
+    throw new Error('file not found')
+  }
+
+  const contents = fs.readFileSync(inputFile, 'utf8')
+
+  const res = await ask(contents)
 
   const msg = res.content[0]
 
   if (msg.type === 'text') {
     console.log(msg.text)
   }
-
-
 }
 
 main().catch(console.error)
--- prompt.ts
+++ prompt.ts
@@ -1,11 +1,9 @@
-
-
 export const prompt = `
 
 You are a HOLE FILLER. You are provided with a program file containing holes, formatted
 as '{{FILL_HERE}}'. 
 
-Your task is to replace the conents of the hole with the appropriate text according to the program context.
+Your task is to replace the conents of the hole with the appropriate text according to the program context. Correct program syntax and indentation should be preserved
 
 All completions MUST be truthful, accurate, well-written and correct.
 
@@ -21,54 +19,6 @@ function addTwo(x) {
   return x + 2
 }
 
-## IMPORTANT: respond with the entire replaced program
-
-`
-
-export const prompt2 = `
-
-You are a HOLE FILLER. You are provided with a file containing holes, formatted
-as '{{HOLE_NAME}}'. 
-
-  Your TASK is to complete with a string to replace this hole
-with, inside a <COMPLETION/> XML tag, including context-aware indentation, if
-needed. All completions MUST be truthful, accurate, well-written and correct.
-
-## EXAMPLE QUERY:
-
-<QUERY>
-function sum_evens(lim) {
-  var sum = 0;
-  for (var i = 0; i < lim; ++i) {
-    {{FILL_HERE}}
-  }
-  return sum;
-}
-</QUERY>
-
-## CORRECT COMPLETION
-
-<COMPLETION>if (i % 2 === 0) {
-      sum += i;
-    }</COMPLETION>
-
-## EXAMPLE QUERY:
-
-<QUERY>
-def sum_list(lst):
-  total = 0
-  for x in lst:
-  {{FILL_HERE}}
-  return total
-
-print sum_list([1, 2, 3])
-</QUERY>
-
-## CORRECT COMPLETION:
-
-<COMPLETION>  total += x</COMPLETION>
-
-
 ## EXAMPLE QUERY:
 
 function hypothenuse(a, b) {
@@ -77,9 +27,33 @@ function hypothenuse(a, b) {
 
 ## CORRECT COMPLETION:
 
-<COMPLETION>a ** 2 + </COMPLETION>
+function hypothenuse(a, b) {
+  return Math.sqrt(a ** 2 + b ** 2);
+}
 
-## IMPORTANT:
+## EXAMPLE QUERY:
 
-- Answer ONLY with the <COMPLETION/> block. Do NOT include anything outside it.
-`
+function sum_evens(lim) {
+  var sum = 0;
+  for (var i = 0; i < lim; ++i) {
+    {{FILL_HERE}}
+  }
+  return sum;
+}
+
+## CORRECT COMPLETION
+
+function sum_evens(lim) {
+  var sum = 0;
+  for (var i = 0; i < lim; ++i) {
+    if (i % 2 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+
+## IMPORTANT: respond with only the entire replaced program and no other comments about it
+
+`;
